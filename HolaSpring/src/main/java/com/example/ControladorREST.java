e864a97deba3bffcf30bab82ebfbdad798ff75cc
sergioAlhama11 <sergio.alhama.11@gmail.com>
2025-11-08 00:08:33 +0100

    New Message Slack

--- HolaSpring/src/main/java/com/example/ControladorREST.java
+++ HolaSpring/src/main/java/com/example/ControladorREST.java
@@ -7,22 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ControladorREST {
+	
+	private static final String MENSAJE = "Hola Mundo con REST en Spring Boot";
 
     // Metodo 1
     @GetMapping("/hello")
     public String saludar() {
-        return "Hola Mundo con REST en Spring Boot";
+        return MENSAJE;
     }
     
     // Metodo 2
     @GetMapping("/hello2")
     public String saludar2() {
-    	return "Hola Mundo con REST en Spring Boot";
+    	return MENSAJE;
     }
     
     // Metodo 3
     @GetMapping("/hello3")
     public String saludar3() {
-    	return "Hola Mundo con REST en Spring Boot";
+    	return MENSAJE;
     }
 }
