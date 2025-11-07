896144de71cdecefbd34d10f6805574cc84d4d9e
sergioAlhama11 <sergio.alhama.11@gmail.com>
2025-11-07 23:51:01 +0100

    Add Comment

--- HolaSpring/src/test/java/com/example/ControladorRESTTest.java
+++ HolaSpring/src/test/java/com/example/ControladorRESTTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+// Tests para la clase ControladorRest
 public class ControladorRESTTest {
 	
 	private ControladorREST controlador;
