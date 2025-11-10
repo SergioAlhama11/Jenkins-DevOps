49a525b94cffc7fffbbc2c77773c26861f326b99
sergioAlhama11 <sergio.alhama.11@gmail.com>
2025-11-10 15:14:12 +0100

    Add new GET endpoint

--- HolaSpring/src/main/java/com/example/ControladorREST.java
+++ HolaSpring/src/main/java/com/example/ControladorREST.java
@@ -8,20 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ControladorREST {
 	
-	private static final String MENSAJE = "Hola Mundo con REST en Spring Boot";
+	private static final String MENSAJE = "Hola Mundo con REST en Spring Boot ";
 
     @GetMapping("/hello")
     public String saludar() {
-        return MENSAJE;
+        return MENSAJE + "1";
     }
     
     @GetMapping("/hello2")
     public String saludar2() {
-    	return MENSAJE;
+    	return MENSAJE + "2";
     }
     
     @GetMapping("/hello3")
     public String saludar3() {
-    	return MENSAJE;
+    	return MENSAJE + "3";
+    }
+    
+    @GetMapping("/hello4")
+    public String saludar4() {
+    	return "Hola Mundo con REST en Spring Boot 4";
     }
 }
